347818be51a82b4d7dde232ade2c01d2639032f9
danielgy <471379891@qq.com>
2018-06-30 14:40:49 +0800

    Update tcn_a.py

--- tcn_a.py
+++ tcn_a.py
@@ -4,14 +4,13 @@ Created on Mon Jun 11 09:22:30 2018
 
 @author: GY
 """
-
 import keras
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from keras.utils import plot_model
 from tcn import tcn
-#from utils import data_generator
+
 
 def data_pre(data):
     """
@@ -30,11 +29,6 @@ def data_pre(data):
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
     return [x_train, y_train, x_test, y_test]
-
-print('> Loading data... ')
-df = pd.read_csv('./data/data(1).csv', header=None)
-X_train, y_train, X_test, y_test = data_pre(df)
-print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 
 
 def run_task():
@@ -88,5 +82,11 @@ def run_task():
     plt.show()
     return model, param_str
 
-if __name__ == '__main__':
+
+if __name__ == '__main__':  
+    print('> Loading data... ')
+    df = pd.read_csv('./data/data(1).csv', header=None)
+    X_train, y_train, X_test, y_test = data_pre(df)
+    print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+
     model, param_str = run_task()
